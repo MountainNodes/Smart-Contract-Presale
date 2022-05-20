712a92d72e41e4c38e1204b3d7b7302e5a58f4b7
PaulHendi <paul.upmc@hotmail.fr>
2022-05-20 02:38:06 +0200

    Added comments

--- contracts/MoutainPresale.sol
+++ contracts/MoutainPresale.sol
@@ -26,18 +26,20 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
   // How many token units a buyer gets per AVAX
   uint256 public rate = 10;
 
-  // The price for a package
-  uint256 public packagePrice = 1; // NOTE : For tests purposes
+  // The price for a package (in AVAX)
+  uint256 public packagePrice = 1* 10 ** 18; // NOTE : For tests purposes
 
+  // Total amount of aMTN that are available for the presale
   uint256 public presaleAmount = 1_000 * 10 ** 18;
 
-// For test purposes
+  // For test purposes (testing PaymentSplitter)
   address[] _payees = [0x29Fd00FA40c90aec39AC604D875907874f237baA, 0xf235695A38Cd517eBB66C3af0217d68a192ed8b0];
-  uint256[] _shares = [30,70];
+  uint256[] _shares = [70,30];
 
   mapping(address => bool) _isWhitelisted;
   
   mapping (address => uint256) tokenHolders;
+
   /**
    * Event for token purchase logging
    * @param purchaser who paid for the tokens
@@ -53,14 +55,12 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
   );
 
  
-  constructor()  
-    ARC20(_name, _symbol)
-    PaymentSplitter(_payees, _shares)
-    {
-  
+  constructor()  ARC20(_name, _symbol) PaymentSplitter(_payees, _shares){
 
+    // Note : the total amount of tokens is minted only once in the constructor
     _mint(address(this), presaleAmount);
   }
+
 
   fallback () external payable {
     buyTokens(_msgSender());
@@ -70,30 +70,35 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
     buyTokens(_msgSender());
  }
     
+  // Checks if the presale has started
   modifier hasStarted()
   {
       require(block.timestamp > _startTime, "Presale has not started yet");
       _;
   }
   
+  // Checks if the presale has not ended
   modifier hasNotEnded()
   {
       require(block.timestamp < _endTime, "Presale has already finished");
       _;
   }
   
+  // Checks that the SC still has tokens 
   modifier hasTokens()
   {
       require (balanceOf(address(this)) > 0 , "No tokens left");
       _;
   }
 
+  // Checks that the SC has not been paused
   modifier isNotPaused()
   {
     require(!paused(), "token transfer while paused");
     _;
   }
   
+  // Checks that vesting is over in order to withdraw the last 50% of tokens 
   modifier isVestingFinished()
   {
       require(_vestingStartTimestamp!=0, "Vesting timestamp has not been set yet");
@@ -102,16 +107,26 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
       
   }
 
-  // StartTime is a Unix time in seconds
+  /**
+   * @param startTime Unix timestamp that define the starting time of the presale
+   */ 
   function setStartTime(uint256 startTime) public onlyOwner {
     _startTime = startTime;
     _endTime = startTime + presaleDuration;
   }
 
+
+  /**
+   * @param vestingStartTime Unix timestamp that define the starting time of the vesting phase
+   */  
   function startVesting(uint256 vestingStartTime) public onlyOwner {
     _vestingStartTimestamp = vestingStartTime;      
   }
 
+  /**
+   * @dev This function is necessary in order to allow investors to purchase tokens
+   * @param addresses All the addresses to be whitelisted
+   */  
   // TODO : Need to optimize the for loop
   function addToWL(address[] memory addresses) public onlyOwner{
       for (uint8 i = 0; i<addresses.length; i++) {
@@ -121,36 +136,41 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
   
   
   /**
-   * @dev low level token purchase ***DO NOT OVERRIDE***
-   */
-   // Need to check that total supply has not been reached before buying
-   // For now has tokens can be used, maybe we should restrict buying package ?
+   * @dev low level token purchase 
+   * @param _beneficiary The address of the investor
+   */  
   function buyTokens(address _beneficiary) public payable {
 
     uint256 weiAmount = msg.value;
 
-    require(weiAmount==packagePrice, "BUY TOKENS : Price not correct");
-
+    // Prevalidate the purchase before buying
     _preValidatePurchase(_beneficiary, weiAmount);
 
     // calculate token amount to be created
     uint256 tokens = _getTokenAmount(weiAmount);
     uint256 _50Percent = tokens/2;
+
+    // Half of the tokens is kept in the SC, mapped to the investor's address
     tokenHolders[_beneficiary] = _50Percent;
-    // update state
+
+    // update state to track the total amount raised
     weiRaised = weiRaised+weiAmount;
     
+    // Send half of the tokens to the investor 
     _deliverTokens(_beneficiary, _50Percent);
-    emit TokenPurchase(
-      msg.sender,
-      _beneficiary,
-      weiAmount,
-      _50Percent
-    );
+
+    // Emit an event to track all purchases
+    emit TokenPurchase(msg.sender, _beneficiary, weiAmount, _50Percent);
 
   }
 
- 
+   /**
+   * @dev With all the modifiers and requires, this function ensures buying tokens is allowed.
+   *      Namely : The presale should be ongoing, there are still tokens in the SC, the SC is not paused,
+   *.     The investor is whitelisted, and the amount is correct.
+   * @param _beneficiary The address of the investor
+   * @param _weiAmount The amount spent by the investor
+   */
   function _preValidatePurchase(
     address _beneficiary,
     uint256 _weiAmount
@@ -159,7 +179,7 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
   {
     require(_beneficiary != address(0), "Buyer addess cannot be 0");
     require(_isWhitelisted[_beneficiary], "Buyer not whitelisted");
-    require(_weiAmount != 0, "Amount cannot be 0");
+    require(_weiAmount==packagePrice, "Price not correct");
   }
 
 
@@ -182,7 +202,10 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
     return _weiAmount*rate;
   }
 
-
+  /**
+   * @dev Once the vesting is finished, investors can withdraw the 50% left of their tokens
+   * @param withdrawer The address of the investor
+   */
   function withdraw (address withdrawer) public isVestingFinished
   {
       require(withdrawer != address(0), "BEP20: Transfer to zero address");
@@ -190,7 +213,9 @@ contract PrivateSale is Ownable, ARC20, Pausable, PaymentSplitter {
       _deliverTokens(withdrawer, withdrawnAmount);
   }
 
-  
+  /**
+   * @dev All unsold tokens can be sent to the owner of the contract, if not sold out
+   */
   function sendTokensBack() public onlyOwner hasNotEnded
   {
     transferFrom(address(this), msg.sender, balanceOf(address(this)));
